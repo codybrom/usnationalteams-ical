6e27bfa13daf7026c85e016a3dc034f0da24b4d3
Cody Bromley <codybrom@users.noreply.github.com>
2023-04-14 18:09:18 -0500

    mnt changes

--- src/pages/api/mnt-calendar.js
+++ src/pages/api/mnt-calendar.js
@@ -13,31 +13,50 @@ export default async (req, res) => {
 
   // Loop through the matches and add them to the calendar
   matches.forEach(match => {
-    const startDate = new Date(match.dateTime);
-    const endDate = new Date(startDate.getTime() + 120 * 60 * 1000); // Assuming 120 minutes per match
+    const homeTeam = match.contestants.find(team => team.position === 'home');
+    const awayTeam = match.contestants.find(team => team.position === 'away');
+  
+    if (!homeTeam || !awayTeam) {
+      console.warn('Skipping match with missing team information:', match);
+      return;
+    }
+  
+    const homeTeamName = homeTeam.name || homeTeam.code;
+    const awayTeamName = awayTeam.name || awayTeam.code;
+  
+    // Replace "USA" and "United States" with "USMNT" in the summary
+    const summary = `${homeTeamName} vs ${awayTeamName}`
+      .replace('USA', 'USMNT')
+      .replace('United States', 'USMNT');
 
-    // Replace "United States" with "USWNT" in the summary
-    const summary = match.description.replace('United States', 'USWNT');
+    const startDate = new Date(match.date);
+    const endDate = new Date(startDate);
+    endDate.setDate(startDate.getDate() + 1); // Set the end date to the next day for all-day events
 
-    // Add competition details and broadcaster names to the description, if available
+    // Check if the time is TBD
+    const isAllDay = match.time.toLowerCase() === 'tbd';
+
+    if (!isAllDay) {
+      const timeParts = match.time.split(':');
+      startDate.setUTCHours(parseInt(timeParts[0]), parseInt(timeParts[1]));
+      endDate.setUTCHours(startDate.getUTCHours() + 2); // Assuming 120 minutes per match
+    }
+
+    // Add competition details to the description, if available
     const competition = match.competition
       ? `Competition: ${match.competition.name}\n`
       : '';
-    const broadcasterNames = match.broadcastLinks
-      ? match.broadcastLinks.map(b => b.imageAltText).join(', ')
-      : 'TBD';
-    const broadcasterLabel = match.broadcastLinks && match.broadcastLinks.length === 1
-      ? 'Broadcaster'
-      : 'Broadcasters';
-    const description = `${competition}${broadcasterLabel}: ${broadcasterNames}`;
+
+    const description = competition;
 
     calendar.createEvent({
       start: startDate,
       end: endDate,
       summary: summary,
       description: description,
-      location: `${match.venue.longName}, ${match.venue.location}, ${match.venue.country}`,
-      url: `https://www.ussoccer.com${match.matchFeedUrl}`, // No label for the link
+      location: `${match.venue.name}, ${match.venue.city}, ${match.venue.country.name}`,
+      url: `https://www.ussoccer.com`, // No match-specific URL available in the JSON
+      allDay: isAllDay,
     });
   });
 
