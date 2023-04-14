df17686a7120c722f022f31fe9713f4706b3b9ac
Cody Bromley <codybrom@users.noreply.github.com>
2023-04-14 17:34:20 -0500

    initial

---
+++ src/pages/api/mnt-calendar.js
@@ -0,0 +1,48 @@
+import ical from 'ical-generator';
+import fetch from 'node-fetch';
+
+const matchesUrl = 'https://www.ussoccer.com/api/matches/upcoming/contestant/9vh2u1p4ppm597tjfahst2m3n';
+
+export default async (req, res) => {
+  // Fetch the matches data from the provided URL
+  const response = await fetch(matchesUrl);
+  const matches = await response.json();
+
+  // Create a new iCal calendar
+  const calendar = ical({ name: 'US Men\'s Soccer Team' });
+
+  // Loop through the matches and add them to the calendar
+  matches.forEach(match => {
+    const startDate = new Date(match.dateTime);
+    const endDate = new Date(startDate.getTime() + 120 * 60 * 1000); // Assuming 120 minutes per match
+
+    // Replace "United States" with "USWNT" in the summary
+    const summary = match.description.replace('United States', 'USWNT');
+
+    // Add competition details and broadcaster names to the description, if available
+    const competition = match.competition
+      ? `Competition: ${match.competition.name}\n`
+      : '';
+    const broadcasterNames = match.broadcastLinks
+      ? match.broadcastLinks.map(b => b.imageAltText).join(', ')
+      : 'TBD';
+    const broadcasterLabel = match.broadcastLinks && match.broadcastLinks.length === 1
+      ? 'Broadcaster'
+      : 'Broadcasters';
+    const description = `${competition}${broadcasterLabel}: ${broadcasterNames}`;
+
+    calendar.createEvent({
+      start: startDate,
+      end: endDate,
+      summary: summary,
+      description: description,
+      location: `${match.venue.longName}, ${match.venue.location}, ${match.venue.country}`,
+      url: `https://www.ussoccer.com${match.matchFeedUrl}`, // No label for the link
+    });
+  });
+
+  // Set the response headers and send the generated iCal data
+  res.setHeader('Content-Type', 'text/calendar;charset=utf-8');
+  res.setHeader('Content-Disposition', 'attachment; filename=calendar.ics');
+  res.send(calendar.toString());
+};
