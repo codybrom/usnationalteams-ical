bdedd12c6ab5261e37b0e02b8074411aff082842
Cody Bromley <codybrom@users.noreply.github.com>
2023-04-14 20:00:32 -0500

    make pages prettier

--- src/pages/api/mnt-calendar.js
+++ src/pages/api/mnt-calendar.js
@@ -9,55 +9,57 @@ export default async (req, res) => {
   const matches = await response.json();
 
   // Create a new iCal calendar
-  const calendar = ical({ name: 'US Men\'s Soccer Team' });
+  const calendar = ical({
+    name: 'US Men\'s Soccer Team'
+  });
 
   // Loop through the matches and add them to the calendar
   matches.forEach(match => {
     const homeTeam = match.contestants.find(team => team.position === 'home');
     const awayTeam = match.contestants.find(team => team.position === 'away');
-  
+
     if (!homeTeam || !awayTeam) {
       console.warn('Skipping match with missing team information:', match);
       return;
     }
-  
+
     const homeTeamName = homeTeam.name || homeTeam.code;
     const awayTeamName = awayTeam.name || awayTeam.code;
-  
+
     // Replace "USA" and "United States" with "USMNT" in the summary
     const summary = `${homeTeamName} vs ${awayTeamName}`
       .replace('USA', 'USMNT')
       .replace('United States', 'USMNT');
 
-      const isAllDay = match.time.toLowerCase() === 'tbd';
+    const isAllDay = match.time.toLowerCase() === 'tbd';
 
-      const startDate = new Date(match.date);
-      const endDate = new Date(match.date);
-      
-      if (isAllDay) {
-        startDate.setUTCHours(0, 0, 0);
-        endDate.setUTCHours(23, 59, 59);
-      } else {
-        const timeParts = match.time.split(':');
-        startDate.setUTCHours(parseInt(timeParts[0]), parseInt(timeParts[1]));
-        endDate.setUTCHours(startDate.getUTCHours() + 2); // Assuming 120 minutes per match
-      }
+    const startDate = new Date(match.date);
+    const endDate = new Date(match.date);
+
+    if (isAllDay) {
+      startDate.setUTCHours(0, 0, 0);
+      endDate.setUTCHours(23, 59, 59);
+    } else {
+      const timeParts = match.time.split(':');
+      startDate.setUTCHours(parseInt(timeParts[0]), parseInt(timeParts[1]));
+      endDate.setUTCHours(startDate.getUTCHours() + 2); // Assuming 120 minutes per match
+    }
 
     // Add competition details and broadcaster names to the description, if available
-    const competition = match.competition
-      ? `Competition: ${match.competition.name}\n`
-      : '';
-    const broadcasterNames = match.broadcastLinks
-      ? match.broadcastLinks.map(b => b.imageAltText).join(', ')
-      : 'TBD';
-    const broadcasterLabel = match.broadcastLinks && match.broadcastLinks.length === 1
-      ? 'Broadcaster'
-      : 'Broadcasters';
+    const competition = match.competition ?
+      `Competition: ${match.competition.name}\n` :
+      '';
+    const broadcasterNames = match.broadcastLinks ?
+      match.broadcastLinks.map(b => b.imageAltText).join(', ') :
+      'TBD';
+    const broadcasterLabel = match.broadcastLinks && match.broadcastLinks.length === 1 ?
+      'Broadcaster' :
+      'Broadcasters';
     const description = `${competition}${broadcasterLabel}: ${broadcasterNames}`;
 
-    const location = match.venue && match.venue.name && match.venue.city && match.venue.country && match.venue.country.name
-  ? `${match.venue.name}, ${match.venue.city}, ${match.venue.country.name}`
-  : 'TBD';
+    const location = match.venue && match.venue.name && match.venue.city && match.venue.country && match.venue.country.name ?
+      `${match.venue.name}, ${match.venue.city}, ${match.venue.country.name}` :
+      'TBD';
 
     calendar.createEvent({
       start: startDate,
